234d2ae49c152811801f9c6fc6df1c0370a21f7f
Gullian <gullianvdwalt1@gmail.com>
2021-09-12 08:31:47 +0200

    feat: repositories

---
+++ src/main/java/com/example/springbootdatatablesserverside/SpringBootDatatablesServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdatatablesserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatatablesServerSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDatatablesServerSideApplication.class, args);
+    }
+
+}
